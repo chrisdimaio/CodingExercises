af67e03e39d4953947b78390e35638e7c05119dc
Chris DiMaio <chrisdimaio@gmail.com>
2015-11-23 12:19:01 -0500

    Created missingIndex().

--- src/main/java/codingexercises/missingindex/MissingIndex.java
+++ src/main/java/codingexercises/missingindex/MissingIndex.java
@@ -4,18 +4,21 @@ import java.util.Arrays;
 
 public class MissingIndex {
 	public static void main(String[] args) {
+		// missing index 4.
 		int[] indexes = {1,2,3,0,5,6};
 
 		System.out.println("Array of indexes: " + Arrays.toString(indexes));
 
+		System.out.println("The missing index is " + missingIndex(indexes) + ".");
+	}
+
+	public static int missingIndex(int[] indexes){
 		int n   = indexes.length;
 		int sum = 0;
 		for(int i = 0; i < indexes.length; i++){
 			sum += indexes[i];
 		}
 
-		int missingIndex = (n * (n + 1) / 2) - sum;
-
-		System.out.println("The missing index is " + missingIndex + ".");
+		return (n * (n + 1) / 2) - sum;
 	}
 }
